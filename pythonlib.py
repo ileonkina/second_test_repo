be2f66a2205dcfdd37963752f430ceae8f2198b2
ileonkina <irinka-leo@yandex.ru>
2012-02-01 12:29:24 +0400

    pffff commit

--- pythonlib.py
+++ pythonlib.py
@@ -1,7 +1,15 @@
 import requests
 import json
-#git_user = 'fakeuser'
-#git_pass = 'gfhjkm2012'
-r = requests.get('https://api.github.com/orgs/TestInternOrg', auth=('fakeuser','gfhjkm2012'))
-print r.content
+git_user = 'fakeuser'
+git_pass = 'gfhjkm2012'
+#r = requests.get('https://api.github.com/orgs/TestInternOrg',auth=(git_user,git_pass))
+#print r.content
+def delUserOrg(log,user_pass):
+	try:
+		r = requests.delete('https://api.github.com/orgs/TestInternOrg/members/'%log,auth=(git_user,git_pass))
+		return r.content
+	except:
+		print "Error"
+		return " "
+	return " "
 
