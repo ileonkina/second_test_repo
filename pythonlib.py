efcbe147d06f5cb046c56c3f808152652ec030c1
ileonkina <irinka-leo@yandex.ru>
2012-02-01 16:07:58 +0400

    First cimmit with some useful functions

--- pythonlib.py
+++ pythonlib.py
@@ -1,15 +1,24 @@
 import requests
 import json
-git_user = 'fakeuser'
-git_pass = 'gfhjkm2012'
-#r = requests.get('https://api.github.com/orgs/TestInternOrg',auth=(git_user,git_pass))
-#print r.content
-def delUserOrg(log,user_pass):
+owner_log='ileonkina'
+owner_pass='19tustuuira89'
+host='https://api.github.com/'
+org_name='TestInternOrg'
+def del_from_org(user):
+	reqq = 'orgs/%s/members/%s' % (org_name,user)
+	url = host + reqq
 	try:
-		r = requests.delete('https://api.github.com/orgs/TestInternOrg/members/'%log,auth=(git_user,git_pass))
-		return r.content
-	except:
-		print "Error"
-		return " "
-	return " "
-
+		r = requests.delete(url,auth = (owner_log,owner_pass))
+	except r.status_code != 200 or r.status_code != 204:
+		return "Error "+ r.headers['status']
+	else:
+		return "Delete successful"
+def add_to_team(user,team_id):
+	reqq = 'teams/%s/members/%s' % (team_id,user)
+	url = host + reqq
+	try:
+		r = requests.put(url,auth = (owner_log,owner_pass),data = '{"login":"%s"}' % user)
+	except r.status_code != 200 or r.status_code != 204:
+		return "Error "+ r.headers['status']
+	else:
+		return "%s was added to team" % user
