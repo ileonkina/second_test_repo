d545e64345893c554d9908f31f3af9ce72332df7
ileonkina <irinka-leo@yandex.ru>
2012-02-02 14:33:54 +0400

    tabs 4

--- pythonlib.py
+++ pythonlib.py
@@ -1,15 +1,15 @@
 import requests
 import json
-owner_log='ileonkina'
-owner_pass='19tustuuira89'
-host='https://api.github.com/'
-org_name='TestInternOrg'
+owner_log = 'ileonkina'
+owner_pass = '19tustuuira89'
+host = 'https://api.github.com/'
+org_name = 'TeInternOrg'
+
 def del_from_org(user):
 	reqq = 'orgs/%s/members/%s' % (org_name,user)
 	url = host + reqq
-	try:
-		r = requests.delete(url,auth = (owner_log,owner_pass))
-	except r.status_code != 200 or r.status_code != 204:
+	r = requests.delete(url,auth = (owner_log,owner_pass))
+	if r.status_code != 204:
 		return "Error "+ r.headers['status']
 	else:
 		return "Delete successful"
@@ -17,9 +17,8 @@ def del_from_org(user):
 def add_to_team(user,team_id):
 	reqq = 'teams/%s/members/%s' % (team_id,user)
 	url = host + reqq
-	try:
-		r = requests.put(url,auth = (owner_log,owner_pass),data = '{"login":"%s"}' % user)
-	except r.status_code != 200 or r.status_code != 204:
+	r = requests.put(url,auth = (owner_log,owner_pass),data = '{"login":"%s"}' % user)
+	if r.status_code != 204:
 		return "Error "+ r.headers['status']
 	else:
 		return "%s was added to team" % user
@@ -27,9 +26,8 @@ def add_to_team(user,team_id):
 def create_team(team_name,permission,repo_name):
 	reqq = 'orgs/%s/teams' % org_name
 	url = host + reqq
-	try:
-		r = requests.post(url,auth = (owner_log,owner_pass),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,org_name,repo_name,permission))
-	except r.status_code != 200 or r.status_code != 204:
+	r = requests.post(url,auth = (owner_log,owner_pass),data = '{"name":"%s", "repo_names":["%s/%s"], "permission":"%s"}' % (team_name,org_name,repo_name,permission))
+	if r.status_code != 201:
 		return "Error "+ r.headers['status']
 	else:
 		return "%s was created" % team_name
